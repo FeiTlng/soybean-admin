38d8e02b75c73b09471dd92038d7e35995f38945
yanjjingdong <1215524238@qq.com>
2025-07-06 01:35:45 +0800

    build: 开发代码去掉提交校验钩子

--- eslint.config.js
+++ eslint.config.js
@@ -4,21 +4,6 @@ export default defineConfig(
   { vue: true, unocss: true },
   {
     rules: {
-      'vue/multi-word-component-names': [
-        'warn',
-        {
-          ignores: ['index', 'App', 'Register', '[id]', '[url]']
-        }
-      ],
-      'vue/component-name-in-template-casing': [
-        'warn',
-        'PascalCase',
-        {
-          registeredComponentsOnly: false,
-          ignores: ['/^icon-/']
-        }
-      ],
-      'unocss/order-attributify': 'off'
     }
   }
 );
