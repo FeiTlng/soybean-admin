77ba7dea96786f6c819690f06810b57aa581bb7b
FeiTlng <1215524238@qq.com>
2025-09-01 00:22:33 +0800

    refactor: 去除调试用输出

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -61,7 +61,6 @@ export function toggleHtmlClass(className: string) {
 export function formatUserRegistryTime(timestamp: any) {
   if (timestamp === null || timestamp === undefined) return "";
   const date = new Date(Number(timestamp));
-  console.log("date", date)
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
