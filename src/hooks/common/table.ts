8b17f21305143b804f6185cf75cfe771d92539af
FeiTlng <1215524238@qq.com>
2025-09-07 00:45:38 +0800

    refactor: 去除调试打印

--- src/hooks/common/table.ts
+++ src/hooks/common/table.ts
@@ -237,7 +237,6 @@ export function useTableOperate<T extends TableData = TableData>(data: Ref<T[]>,
     operateType.value = 'edit';
     const findItem = data.value.find(item => item.id === id) || null;
     editingData.value = jsonClone(findItem);
-    console.log("editingData.value", editingData.value)
 
     openDrawer();
   }
